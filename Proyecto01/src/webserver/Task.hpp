f909cab777522334758dd7d4afa1f45893290632
Aaron Campos <aaron.camposramirez@ucr.ac.cr>
2021-07-22 22:17:36 -0600

    Correciones linter

--- Proyecto01/src/webserver/Task.hpp
+++ Proyecto01/src/webserver/Task.hpp
@@ -5,22 +5,21 @@
 #include"HttpResponse.hpp"
 #include<vector>
 class Task{
-    public:
-        Task();
-        Task(HttpRequest* request, HttpResponse* response, int64_t number,
-        size_t index);
-        Task(const Task& other);
-        ~Task();
  public:
-        HttpRequest* request;
-        HttpResponse* response;
-        int64_t number;
-        size_t index;
-        std::string resultSums;
-         /// Returns true if this socket represents
-         // clear
-         the same connection than the other
-        bool operator==(const Task& other) const;
+  Task();
+  Task(HttpRequest* request, HttpResponse* response, int64_t number,
+  size_t index);
+  Task(const Task& other);
+  ~Task();
+ public:
+  HttpRequest* request;
+  HttpResponse* response;
+  int64_t number;
+  size_t index;
+  std::string resultSums;
+  /// Returns true if this socket represents 
+  //the same connection than the other
+  bool operator==(const Task& other) const; 
 };
 
 #endif
